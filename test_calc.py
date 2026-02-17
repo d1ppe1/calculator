e181e4aaea070a9060b7581b6b6ec3986ebfa343
d1ppe1 <nataprogame2022@mail.ru>
2026-02-17 15:29:01 +0900

    Исправлены тесты под графический калькулятор

--- test_calc.py
+++ test_calc.py
@@ -1,29 +1,119 @@
 import pytest
-from calc import add, subtract, multiply, divide
+import math
+from calc import Calculator
+import tkinter as tk
 
-def test_add():
-    """Тест сложения"""
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
-    assert add(0, 0) == 0
-
-def test_subtract():
-    """Тест вычитания"""
-    assert subtract(5, 3) == 2
-    assert subtract(0, 5) == -5
-    assert subtract(10, 10) == 0
-
-def test_multiply():
-    """Тест умножения"""
-    assert multiply(4, 3) == 12
-    assert multiply(-2, 3) == -6
-    assert multiply(0, 5) == 0
-
-def test_divide():
-    """Тест деления"""
-    assert divide(10, 2) == 5.0
-    assert divide(5, 2) == 2.5
+class TestCalculator:
+    """Тесты для графического калькулятора"""
     
-    # Проверка исключения при делении на ноль
-    with pytest.raises(ValueError):
-        divide(5, 0)
+    def setup_method(self):
+        """Создаем экземпляр калькулятора перед каждым тестом"""
+        self.root = tk.Tk()
+        self.root.withdraw()  # Скрываем окно
+        self.calc = Calculator(self.root)
+    
+    def teardown_method(self):
+        """Уничтожаем окно после каждого теста"""
+        self.root.destroy()
+    
+    def test_initial_state(self):
+        """Тест начального состояния"""
+        assert self.calc.result_var.get() == "0"
+        assert self.calc.current_input == ""
+    
+    def test_number_input(self):
+        """Тест ввода цифр"""
+        self.calc.button_click('1')
+        assert self.calc.current_input == "1"
+        assert self.calc.result_var.get() == "1"
+        
+        self.calc.button_click('2')
+        assert self.calc.current_input == "12"
+        assert self.calc.result_var.get() == "12"
+    
+    def test_clear(self):
+        """Тест очистки (C)"""
+        self.calc.button_click('1')
+        self.calc.button_click('2')
+        self.calc.button_click('C')
+        assert self.calc.current_input == ""
+        assert self.calc.result_var.get() == "0"
+    
+    def test_backspace(self):
+        """Тест удаления последнего символа (⌫)"""
+        self.calc.button_click('1')
+        self.calc.button_click('2')
+        self.calc.button_click('3')
+        self.calc.button_click('⌫')
+        assert self.calc.current_input == "12"
+        assert self.calc.result_var.get() == "12"
+    
+    def test_addition(self):
+        """Тест сложения через calculate_result"""
+        self.calc.button_click('2')
+        self.calc.button_click('+')
+        self.calc.button_click('3')
+        self.calc.calculate_result()
+        assert self.calc.result_var.get() == "5"
+    
+    def test_subtraction(self):
+        """Тест вычитания"""
+        self.calc.button_click('5')
+        self.calc.button_click('-')
+        self.calc.button_click('2')
+        self.calc.calculate_result()
+        assert self.calc.result_var.get() == "3"
+    
+    def test_multiplication(self):
+        """Тест умножения"""
+        self.calc.button_click('4')
+        self.calc.button_click('*')
+        self.calc.button_click('3')
+        self.calc.calculate_result()
+        assert self.calc.result_var.get() == "12"
+    
+    def test_division(self):
+        """Тест деления"""
+        self.calc.button_click('1')
+        self.calc.button_click('0')
+        self.calc.button_click('/')
+        self.calc.button_click('2')
+        self.calc.calculate_result()
+        assert self.calc.result_var.get() == "5.0"
+    
+    def test_square_root(self):
+        """Тест квадратного корня"""
+        self.calc.button_click('9')
+        self.calc.calculate_square_root()
+        assert float(self.calc.result_var.get()) == 3.0
+    
+    def test_square_root_error(self):
+        """Тест корня из отрицательного числа"""
+        self.calc.button_click('-')
+        self.calc.button_click('9')
+        self.calc.calculate_square_root()
+        assert "Ошибка" in self.calc.result_var.get()
+    
+    def test_power(self):
+        """Тест возведения в степень (через ^)"""
+        self.calc.button_click('2')
+        self.calc.button_click('^')
+        self.calc.button_click('3')
+        self.calc.calculate_result()
+        # 2**3 = 8
+        assert self.calc.result_var.get() == "8"
+    
+    def test_complex_expression(self):
+        """Тест сложного выражения"""
+        # 2+3*4-5/2 = 2+12-2.5 = 11.5
+        self.calc.button_click('2')
+        self.calc.button_click('+')
+        self.calc.button_click('3')
+        self.calc.button_click('*')
+        self.calc.button_click('4')
+        self.calc.button_click('-')
+        self.calc.button_click('5')
+        self.calc.button_click('/')
+        self.calc.button_click('2')
+        self.calc.calculate_result()
+        assert float(self.calc.result_var.get()) == 11.5
